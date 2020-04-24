b9f68984d6a0b731ed18d166fdadf5e94ed26e8f
liuyuqi-dellpc <liuyuqi.gov@msn.cn>
2020-04-25 07:04:56 +0800

    增加打开链接按钮

--- app/src/main/java/me/yoqi/qrcode/ResultActivity.java
+++ app/src/main/java/me/yoqi/qrcode/ResultActivity.java
@@ -1,37 +1,57 @@
 package me.yoqi.qrcode;
 
-import me.yoqi.qrcode.R;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import me.yoqi.qrcode.utils.StringUtils;
 
 public class ResultActivity extends Activity {
 
-	private EditText et_result;
-	ClipboardManager myClipboard;
+    private EditText et_result;
+    private Button btn_url;
+    ClipboardManager myClipboard;
+    String text;
 
-	@SuppressLint("NewApi")
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_result);
-		initView();
-		Intent intent = getIntent();
-		String text = intent.getStringExtra("text");
-		et_result.setText(text);
-		
-		myClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
-		
-		ClipData myClip = ClipData.newPlainText("text", text);
-		myClipboard.setPrimaryClip(myClip);
-	}
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        initView();
+        Intent intent = getIntent();
+        text = intent.getStringExtra("text");
+        et_result.setText(text);
+        if (StringUtils.isHttpUrl(text)) {
+            btn_url.setVisibility(View.VISIBLE);
+        } else {
+            btn_url.setVisibility(View.GONE);
+        }
 
-	private void initView() {
-		et_result = (EditText) findViewById(R.id.et_result);
-	}
+        myClipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        ClipData myClip = ClipData.newPlainText("text", text);
+        myClipboard.setPrimaryClip(myClip);
+    }
+
+    private void initView() {
+        et_result = (EditText) findViewById(R.id.et_result);
+        btn_url = (Button) findViewById(R.id.btn_url);
+        btn_url.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setData(Uri.parse(text));//Url 就是你要打开的网址
+                intent.setAction(Intent.ACTION_VIEW);
+                startActivity(intent); //启动浏览器
+            }
+        });
+    }
 }
