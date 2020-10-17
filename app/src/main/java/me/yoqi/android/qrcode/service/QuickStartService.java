9a7c7cec918145846afc75467e190341367fe7a2
liuyuqi-dellpc <liuyuqi.gov@msn.cn>
2020-10-17 18:15:18 +0800

    增加 sp存储，没用功能

--- app/src/main/java/me/yoqi/android/qrcode/service/QuickStartService.java
+++ app/src/main/java/me/yoqi/android/qrcode/service/QuickStartService.java
@@ -2,11 +2,14 @@ package me.yoqi.android.qrcode.service;
 
 import android.content.Intent;
 import android.os.Build;
+import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
 import androidx.annotation.RequiresApi;
 
 import me.yoqi.android.qrcode.CaptureActivity;
+import me.yoqi.android.utils.SPUtils;
+import me.yoqi.android.utils.SimplexToast;
 
 /**
  * 下拉快速启动
@@ -16,6 +19,14 @@ import me.yoqi.android.qrcode.CaptureActivity;
  */
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class QuickStartService extends TileService {
+    Tile tile;
+    SPUtils spUtils;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        spUtils = SPUtils.getInstance(this);
+    }
 
     /**
      * 当用户从Edit栏添加到快速设置中调用
@@ -35,14 +46,34 @@ public class QuickStartService extends TileService {
     @Override
     public void onClick() {
         super.onClick();
-        int state = getQsTile().getState();
+        tile = getQsTile();
+        int state = tile.getState();
+        if (state == Tile.STATE_INACTIVE) {
+            tile.setState(Tile.STATE_ACTIVE);
+            spUtils.putBoolean("flag_qrcode", true);
+        } else {
+            spUtils.putBoolean("flag_qrcode", false);
+        }
+        tile.updateTile();
+
+        SimplexToast.show(this, state + "");
         Intent intent = new Intent(this, CaptureActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivityAndCollapse(intent);
     }
 
+    /**
+     * 打开下拉菜单的时候调用,当快速设置按钮并没有在编辑栏拖到设置栏中不会调用
+     */
     @Override
     public void onStartListening() {
         super.onStartListening();
+        tile = getQsTile();
+        if (tile == null) {
+            return;
+        }
+        boolean flag = spUtils.getBoolean("flag_qrcode", false); // 是否启动
+        tile.setState(flag ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.updateTile();
     }
 }
---
+++ app/src/main/java/me/yoqi/android/utils/StringUtils.java
@@ -0,0 +1,10 @@
+package me.yoqi.android.utils;
+
+/**
+ * @author liuyuqi.gov@msn.cn
+ * @createTime 2020-08-17
+ */
+public class StringUtils {
+
+}
+
